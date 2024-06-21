53e43a9444ca200a39e94a6593c109cb63502fd7
MessLeader <168623430+22f1000870@users.noreply.github.com>
2024-06-21 17:02:19 +0530

    influencer registration done

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 
 from flask import Flask,render_template,request, url_for,redirect
 from model11 import*
+import os
 
 app=Flask(__name__)
 
-app.config['SQL_ALCHEMY_DATABASE_URI']="sqlite:///project_database.sqlite3"
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///project_database.sqlite3"
 db.init_app(app)
 
 app.app_context().push()
 db.create_all()
 
-@app.route("/",methods=["POST","GET"])
+@app.route("/",methods=["GET"])
 def home():
     if request.method=="GET":
         return render_template("login.html")
-    else:
-        return redirect(url_for("login"))
 
 @app.route("/registration/<usertype>",methods=["GET","POST"])
 def registration(usertype):
     if request.method=="GET":
         if usertype=="influencer":
-            return render_template("registration1.html",user=usertype)
+            return render_template("registration1.html")
         else:
-            return render_template("registration1.html",user=usertype)
+            return render_template("registration.html")
     else:
-        pass
+        if usertype=='influencer':
+
+            a=request.form
+            u=Roles(username=a['username'],password=a['pswd'],type=usertype)
+            db.session.add(u)
+            db.session.flush()
+            r=int(a['instagram'])+int(a['facebook'])+int(a['youtube'])
+            f=(request.files['image']).read()
+            i=Influencer(influencer_id=u.user_id,fname=a['fname'],lname=a['lname'],image=f,reach=r)
+            db.session.add(i)
+            db.session.commit()
+            return redirect(url_for('home'))
+            
+
+
 if __name__=="__main__":
     app.run(debug=True)
 
--- model11.py
+++ model11.py
@@ -2,52 +2,56 @@ from flask_sqlalchemy import SQLAlchemy
 
 db=SQLAlchemy()
 
-class roles(db.Model):
+class Roles(db.Model):
     __tablename__='roles'
-    user_id=db.Column(db.Integer,autoincrement=True,unique=True)
-    username=db.Column(db.String,primary_key=True)
+    user_id=db.Column(db.Integer,primary_key=True,autoincrement=True,unique=True)
+    username=db.Column(db.String,unique=True,nullable=False)
     password=db.Column(db.String,nullable=False)
     type=db.Column(db.String,nullable=False)
     
+    sponsor=db.relationship('Sponsor',uselist=False,backref=db.backref('role',uselist=False),cascade='all,delete-orphan')
+    influencer=db.relationship('Influencer',uselist=False,backref=db.backref('role',uselist=False),cascade='all,delete-orphan')
+
 
 class Sponsor(db.Model):
     __tablename__='sponsor'
-    sponsor_id=db.Column(db.Integer,db.ForiegnKey('roles.user_id'))
+    sponsor_id=db.Column(db.Integer,db.ForeignKey('roles.user_id'),primary_key=True)
     company_name=db.Column(db.String,nullable=False,unique=True)
     industry=db.Column(db.String,nullable=False)
     image=db.Column(db.LargeBinary,nullable=False)
     budget=db.Column(db.Double,nullable=False)
-    role=db.relationship('Role',uselist=False,backref=db.backref('sponsor',uselist=False,lazy='dynamic'),lazy='dynamic')
-    campaign=db.relationship('Campaign',backref=db.backref('sponsor',lazy='dynamic'),lazy='dynamic')
+    
+    campaign=db.relationship('Campaign',backref=db.backref('sponsor'))
+    
 
 class Campaign(db.Model):
     __tablename__='campaign'
+    sponsor_id=db.Column(db.Integer,db.ForeignKey('sponsor.sponsor_id'))
     campaign_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
     niche=db.Column(db.String,nullable=False)
     requirement=db.Column(db.String)
     visibility=db.Column(db.Integer,nullable=False)
     amount=db.Column(db.Double,nullable=False)
-    time=db.relationship('Time',uselist=False,backref=db.backref('campaign',uselist=False,lazy='dynamic'),lazy='dynamic')
+    time=db.relationship('Time',uselist=False,backref=db.backref('campaign',uselist=False))
     
     
 class Influencer(db.Model):
     __tablename__='influencer'
-    influencer_id=db.Column(db.Integer,db.ForeignKey('roles.user_id'))
+    influencer_id=db.Column(db.Integer,db.ForeignKey('roles.user_id'),primary_key=True)
     fname=db.Column(db.String,nullable=False)
     lname=db.Column(db.String)
     image=db.Column(db.LargeBinary,nullable=False)
     reach=db.Column(db.Integer,nullable=False)
-    role=db.relationship('Role',uselist=False,backref=db.backref('influencer',uselist=False,lazy='dynamic'),lazy='dynamic')
-
+    
 
 
 class Time(db.Model):
-    campaign_id=db.Column(db.Integer,db.ForeignKey('campaign.campaign_id'))
+    campaign_id=db.Column(db.Integer,db.ForeignKey('campaign.campaign_id'),primary_key=True)
     start=db.Column(db.DateTime,nullable=False)
     end=db.Column(db.DateTime,nullable=False)
     status=db.Column(db.Integer,nullable=False)
     
 class CI(db.Model):
-    campaign_id=db.Column(db.Integer,db.ForeignKey('campaign.campaign_id'))
+    campaign_id=db.Column(db.Integer,db.ForeignKey('campaign.campaign_id'),primary_key=True)
     influencer_id=db.Column(db.Integer,db.ForeignKey('influencer.influencer_id'))
     
